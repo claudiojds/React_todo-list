d3b3caa4d94e70773efe6a428e49828fa11df741
Claudio Jose <claudio.jose.sousaa@gmail.com>
2024-07-30 23:29:33 -0300

    Criando lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/header/index"
+import { TodoList } from "./components/todoList"
 import { useSaveTodos } from "./hook/useSaveTodos"
 import styles from './style/App.module.css'
 
@@ -11,6 +12,10 @@ const App = () => {
       <header>
         <Header/>
       </header>
+
+      <main>
+        <TodoList/>
+      </main>
     </div>
   )
 }
